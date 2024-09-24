fdc21806024ea4ff649bba9841fd073adc937ddc
Okekunle Opeyemi <okekunleopeyemi1@gmail.com>
2024-09-24 15:09:53 +0100

    update

--- flaskapp.py
+++ flaskapp.py
@@ -3,33 +3,31 @@ import numpy as np
 import joblib
 import pandas as pd
 
+
 def return_prediction(model, scaler, col_name, data):
-    # Extract data from JSON
     Gender = data['gender']
-    Marital_s = data['marital_status']
-    No_Dependent = data['no_of_dependent']
     Type_residence = data['type_of_residence']
     Education = data['educational_attainment']
     Employment = data['employment_status']
     Sector = data['sector_of_employment']
     Amount = data['requested_amount']
     Purposeofloan = data['purpose']
+    Loan_day = data['loan_request_day']
     Age = data['age']
     Id = data['selfie_id_check']
     No_loans = data['loans']
     Telephone = data['phone_numbers']
     Os = data['mobile_os']
-    Months_E = data['months_employed']
     Income = data['income_range']
 
-    cat_df = pd.DataFrame([[Gender, Marital_s, Type_residence, Education, Employment, Sector, Purposeofloan, Id, Os, Income]],
-                          columns=['gender', 'marital_status', 'type_of_residence', 'educational_attainment', 'employment_status',
-                                   'sector_of_employment', 'purpose', 'selfie_id_check', 'mobile_os', 'income_range'])
+    cat_df = pd.DataFrame([[Gender, Type_residence, Education, Employment, Sector, Purposeofloan, Loan_day, Id, Os, Income]],
+                          columns=['gender', 'type_of_residence', 'educational_attainment', 'employment_status',
+                                   'sector_of_employment', 'purpose', 'loan_request_day', 'selfie_id_check', 'mobile_os', 'income_range'])
     
     cat_encoded = pd.get_dummies(cat_df, drop_first=True)
 
-    num_df = pd.DataFrame([[No_Dependent, Amount, Age, No_loans, Months_E]],
-                          columns=['no_of_dependent', 'requested_amount', 'age', 'loans', 'months_employed'])
+    num_df = pd.DataFrame([[Amount, Age, No_loans, Telephone]],
+                          columns=['no_of_dependent', 'age', 'loans', 'phone_numbers'])
 
     df = pd.concat([num_df, cat_encoded], axis=1)
     
@@ -43,17 +41,19 @@ def return_prediction(model, scaler, col_name, data):
     else:
         return 'Not paid'
 
+cat_feat = ['gender','employment_status','sector_of_employment','loan_request_day','selfie_id_check','type_of_residence', 'educational_attainment','purpose','mobile_os','income_range']
 
+num_feat = ['age','requested_amount','loans','phone_numbers']
 
 lendsqr = Flask(__name__)
 
 @lendsqr.route("/")
 def index():
-    return '<h1>LENDSQR FLASK IS RUNNING</h1>'
+    return '<h1>LENDSQR LOAN APP IS RUNNING</h1>'
 
 lr_model = joblib.load('ldsqr_lr_model.pkl')
 lr_scaler = joblib.load("ldsqr_scaler.pkl")
-col_name = joblib.load("ldsqr_col_name.pkl")
+col_name = joblib.load("col_name.pkl")
 
 @lendsqr.route('/loan_predict', methods=['POST'])
 def loan_prediction():
